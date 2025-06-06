d08ff7cdb84b70376761b8b0d7b48cc003358712
Markus Ort <markus@ort.dev>
2025-06-06 20:20:05 +0200

    add openscad language server

--- init.lua
+++ init.lua
@@ -195,7 +195,7 @@ require("lazy").setup({
     { -- Fuzzy Finder (files, lsp, etc)
         'nvim-telescope/telescope.nvim',
         event = 'VimEnter',
-        branch = '0.1.x',
+        -- branch = '0.1.x',
         dependencies = {
             'nvim-lua/plenary.nvim',
             { -- If encountering errors, see telescope-fzf-native README for install instructions
@@ -300,7 +300,6 @@ require("lazy").setup({
     },
     { -- LSP Configuration & Plugins
         'neovim/nvim-lspconfig',
-
         opts = {
             inlay_hints = { enabled = true },
         },
@@ -316,35 +315,6 @@ require("lazy").setup({
             { 'j-hui/fidget.nvim', opts = {} },
         },
         config = function()
-            -- Brief Aside: **What is LSP?**
-            --
-            -- LSP is an acronym you've probably heard, but might not understand what it is.
-            --
-            -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-            -- and language tooling communicate in a standardized fashion.
-            --
-            -- In general, you have a "server" which is some tool built to understand a particular
-            -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc). These Language Servers
-            -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-            -- processes that communicate with some "client" - in this case, Neovim!
-            --
-            -- LSP provides Neovim with features like:
-            --  - Go to definition
-            --  - Find references
-            --  - Autocompletion
-            --  - Symbol Search
-            --  - and more!
-            --
-            -- Thus, Language Servers are external tools that must be installed separately from
-            -- Neovim. This is where `mason` and related plugins come into play.
-            --
-            -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-            -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-            --  This function gets run when an LSP attaches to a particular buffer.
-            --    That is to say, every time a new file is opened that is associated with
-            --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-            --    function will be executed to configure the current buffer
             vim.diagnostic.config({
                 signs = true
             })
@@ -456,7 +426,6 @@ require("lazy").setup({
                         }
                     }
                 },
-                ['openscad-lsp'] = {},
                 jsonls = {
                     settings = {
                         json = {
@@ -465,6 +434,7 @@ require("lazy").setup({
                         }
                     }
                 },
+                ['openscad-language-server'] = {},
                 rust_analyzer = {
                     settings = {
                         ["rust-analyzer"] = {
@@ -564,6 +534,7 @@ require("lazy").setup({
             require("lsp_lines").setup()
             vim.diagnostic.config({
                 virtual_text = false,
+                virtual_lines = true,
             })
         end,
     }
